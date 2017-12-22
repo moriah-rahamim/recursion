37e97066895b1090a9b89545e1b01863ffda68f7
Moriah Rahamim <moriahfaye@gmail.com>
2017-12-21 20:36:23 -0500

    added support for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -31,4 +31,35 @@ var stringifyJSON = function(obj) {
   // ======================
   //
   
+  // Array
+  // NOTE: For arrays, JSON.stringify does NOT include all enumerable properties.
+  // It only includes indexed properties starting at 0.
+  if (Array.isArray(obj)) {
+    let pieces = [];
+
+    for (let i = 0; i < obj.length; i++) {
+      let item = obj[i];
+      let itemType = typeof item;
+
+      // undefined, a function, or a symbol is censored to null when found in an array.
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#Description
+      if (item === undefined || itemType  === 'function' || itemType === 'symbol') {
+        pieces.push(stringifyJSON(null));
+      } else {
+        pieces.push(stringifyJSON(item));
+      }
+    }
+
+    return '[' + pieces.join(',') + ']';
+  }
+
+  // Object
+  if (type === 'object' ) {
+    // placeholder
+    return '{}';
+  }
+
+  // If it doesn't match any of the above types, then who the hell knows what this is
+  // But we're not doing anything special with it. :)
+  return '';
 };
